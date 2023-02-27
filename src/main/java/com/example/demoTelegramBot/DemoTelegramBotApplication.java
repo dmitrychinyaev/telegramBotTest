1b288c9300634a4bb0a6cf7972149fd381c8d8f2
Dmitry <dmitrychinyaev93@yandex.ru>
2023-02-28 00:21:22 +0300

    Initializer

---
+++ src/main/java/com/example/demoTelegramBot/DemoTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoTelegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoTelegramBot/DemoTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoTelegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
